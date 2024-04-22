c78c3beb120cd5aeedadb3ba4d4b9cc96dbdd2a6
Guccifer808 <hucciallstars@gmail.com>
2024-04-22 21:26:40 +0300

    Fixed container for blog page

--- components/BlogList/BlogList.jsx
+++ components/BlogList/BlogList.jsx
@@ -7,18 +7,16 @@ const BlogList = () => {
   return (
     <>
       <section className='wrapper container wow fadeInUp' data-wow-delay='.5s'>
-        <div className='container'>
-          {/* <div className='section-title text-center mb-60'>
+        {/* <div className='section-title text-center mb-60'>
             <span className='sub-title '>Наш Блог</span>
             <h2 className='title '>
               <span>Полезный</span> заголовок секции
             </h2>
           </div> */}
-          <div className='row'>
-            {blogSectionData.map((post, index) => (
-              <BlogCard key={index} post={post} />
-            ))}
-          </div>
+        <div className='row'>
+          {blogSectionData.map((post, index) => (
+            <BlogCard key={index} post={post} />
+          ))}
         </div>
       </section>
     </>
